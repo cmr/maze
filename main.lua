88eb65ae454952694405acbc06981f0ff0e8cfee
Corey Richardson <corey@octayn.net>
2012-12-12 16:10:34 -0500

    It works!
    But just barely! It's very dumb at the moment. And it appears to not work at
    all when coupled wit LuaJIT, or at least work in a very mysterious manner not
    at all related to the logic I think I wrote.
    
    Very messy. I need to bin the whole thing and start from scratch. But that's
    ok. :shipit:

--- main.lua
+++ main.lua
@@ -1,3 +1,5 @@
+local generator
+
 function love.load()
 	maze = require 'maze'
 	m = maze:new(30, 40)
@@ -13,7 +15,10 @@ dt_tot = 0
 function love.update(dt)
 	dt_tot = dt_tot + dt
 	if dt_tot > 0.5 then
-		coroutine.resume(generator)
+		val = coroutine.resume(generator)
+		if val ~= true then
+			love.update = nil
+		end
 		dt_tot = 0
 	end
 end
--- maze.lua
+++ maze.lua
@@ -2,26 +2,22 @@ local maze = {}
 local maze_mt = {__index = maze}
 
 function makepair(tab, x)
+	local y
 	if x == nil then
-		local x = position.x
-		local y = position.y
+		x = tab.x
+		y = tab.y
 	else
-		local x = tab
-		local y = x
+		x = tab
+		y = x
 	end
 	return "(" .. x .. ", " .. y .. ")"
 end
 
 local directions = {
-	-- Each of these is a pair of offsets for each of the 8 directions that can
-	-- be moved in in a square grid.
+	-- N/S/E/W offsets
 	{x = 0, y = 1},
 	{x = 0, y = -1},
-	{x = 1, y = 1},
-	{x = 1, y = -1},
 	{x = 1, y = 0},
-	{x = -1, y = 1},
-	{x = -1, y = -1},
 	{x = -1, y = 0}
 }
 
@@ -31,7 +27,7 @@ function maze:new(x, y)
 	for x_ = 1, x do
 		t.grid[x_] = {}
 		for y_ = 1, y do
-			t.grid[x_][y_] = 0
+			t.grid[x_][y_] = false
 		end
 	end
 
@@ -50,27 +46,45 @@ function maze:locOnBorder(x, y)
 	return false
 end
 
-function maze:set(x, y)
-	self.grid[x][y] = 1
+function maze:set(x, y, val)
+	if val == nil then val = true end
+	self.grid[x][y] = val
 	return
 end
 
+function maze:get(x, y)
+	-- todo: make this less fragileish
+	if x < 1 or y < 1 then
+		return false
+	elseif x > self.x or y > self.y then
+		return false
+	end
+
+	return self.grid[x][y]
+end
+
 function maze:clear(x, y)
-	self.grid[x][y] = 0
+	self.grid[x][y] = false
 	return
 end
 
 function maze:draw()
 	for x, ya in ipairs(self.grid) do
 		for y,val in ipairs(ya) do
-			love.graphics.setColor(255, 255, 255)
-			if val == 1 then love.graphics.rectangle('fill', x*5, y*5, 4, 4) end
+			if val then
+				if val == 'done' then
+					love.graphics.setColor(255, 0, 0)
+				end
+				love.graphics.rectangle('fill', x*5, y*5, 4, 4)
+				love.graphics.setColor(255, 255, 255)
+			end
 		end
 	end
 end
 
 -- todo: this feels ugly
 local generate = function(self)
+	print("generate() called")
 	--[[
 		1. pick a random point on the edge
 		2. do a random walk, not walking over previously visited squares, until
@@ -84,7 +98,7 @@ local generate = function(self)
 	if x_or_y == 0 then
 		-- x
 		if side == 0 then
-			position.x = 0
+			position.x = 1
 		else
 			position.x = self.x
 		end
@@ -92,7 +106,7 @@ local generate = function(self)
 	else
 		-- y
 		if side == 0 then
-			position.y = 0
+			position.y = 1
 		else
 			position.y = self.y
 		end
@@ -101,28 +115,38 @@ local generate = function(self)
 
 	self:set(position.x, position.y)
 
+	coroutine.yield(position)
+
 	-- (2) Random walk!
 	
 	-- walks a single step. returns false if not able to go further
 	function walk()
 		local good_directions = {}
-		for _,offset in directions do
+		for _,offset in ipairs(directions) do
+			print( self:get(position.x + offset.x, position.y + offset.y))
 			if not self:get(position.x + offset.x, position.y + offset.y) then
-				good_directions.insert(offset)
+				table.insert(good_directions, offset)
 			end
 		end
 		
-		if #good_directions == 0 then return false end
-		
+		if #good_directions == 0 then
+			love.graphics.setColor(255, 0, 0)
+			return false
+		end
+
+		print(#good_directions)
 		local direction = good_directions[math.random(1, #good_directions)]
-		position.x = position.x + offset.x
-		position.y = position.y + offset.y
+		position.x = position.x + direction.x
+		position.y = position.y + direction.y
 		
 		if position.x > self.x or position.y > self.y or position.x < 1 or
 		   position.y < 1 then
+		   self:set(position.x - direction.x, position.y - direction.y, 'done')
 		   return 'done'
 		end
 
+		coroutine.yield(position)
+		print("Going to " .. makepair(position))
 		self:set(position.x, position.y)
 		return true
 	end
@@ -131,12 +155,12 @@ local generate = function(self)
 	while true do
 		local st = walk()
 		if st == 'done' then
+			coroutine.yield(st)
 			return true
 		end
 		if st == false then
-			return false
+			coroutine.yield(false)
 		end
-		coroutine.yield()
 	end
 end
 
