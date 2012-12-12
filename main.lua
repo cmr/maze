ec0cafcf245108c441b36b6fea67965577971b27
Corey Richardson <corey@octayn.net>
2012-12-12 00:30:10 -0500

    Add basic generator
    Currently unfunctioning, need to whack at it with a debugger.

--- main.lua
+++ main.lua
@@ -1,10 +1,19 @@
 function love.load()
-	print("love.load called\n")
 	maze = require 'maze'
 	m = maze:new(30, 40)
-	m:generate()
+	generator = m:generate()
 end
 
 function love.draw()
 	m:draw()
+end
+
+dt_tot = 0
+
+function love.update(dt)
+	dt_tot = dt_tot + dt
+	if dt_tot > 0.5 then
+		coroutine.resume(generator)
+		dt_tot = 0
+	end
 end
--- maze.lua
+++ maze.lua
@@ -1,6 +1,29 @@
 local maze = {}
-
 local maze_mt = {__index = maze}
+
+function makepair(tab, x)
+	if x == nil then
+		local x = position.x
+		local y = position.y
+	else
+		local x = tab
+		local y = x
+	end
+	return "(" .. x .. ", " .. y .. ")"
+end
+
+local directions = {
+	-- Each of these is a pair of offsets for each of the 8 directions that can
+	-- be moved in in a square grid.
+	{x = 0, y = 1},
+	{x = 0, y = -1},
+	{x = 1, y = 1},
+	{x = 1, y = -1},
+	{x = 1, y = 0},
+	{x = -1, y = 1},
+	{x = -1, y = -1},
+	{x = -1, y = 0}
+}
 
 function maze:new(x, y)
 	local t = setmetatable({x = x, y = y, grid={}}, maze_mt)
@@ -29,21 +52,25 @@ end
 
 function maze:set(x, y)
 	self.grid[x][y] = 1
+	return
 end
 
 function maze:clear(x, y)
 	self.grid[x][y] = 0
+	return
 end
 
 function maze:draw()
 	for x, ya in ipairs(self.grid) do
 		for y,val in ipairs(ya) do
+			love.graphics.setColor(255, 255, 255)
 			if val == 1 then love.graphics.rectangle('fill', x*5, y*5, 4, 4) end
 		end
 	end
 end
 
-function maze:generate()
+-- todo: this feels ugly
+local generate = function(self)
 	--[[
 		1. pick a random point on the edge
 		2. do a random walk, not walking over previously visited squares, until
@@ -71,9 +98,50 @@ function maze:generate()
 		end
 		position.x = math.random(1, self.x)
 	end
-	
+
 	self:set(position.x, position.y)
-	
+
 	-- (2) Random walk!
 	
+	-- walks a single step. returns false if not able to go further
+	function walk()
+		local good_directions = {}
+		for _,offset in directions do
+			if not self:get(position.x + offset.x, position.y + offset.y) then
+				good_directions.insert(offset)
+			end
+		end
+		
+		if #good_directions == 0 then return false end
+		
+		local direction = good_directions[math.random(1, #good_directions)]
+		position.x = position.x + offset.x
+		position.y = position.y + offset.y
+		
+		if position.x > self.x or position.y > self.y or position.x < 1 or
+		   position.y < 1 then
+		   return 'done'
+		end
+
+		self:set(position.x, position.y)
+		return true
+	end
+	
+	-- do all the walking
+	while true do
+		local st = walk()
+		if st == 'done' then
+			return true
+		end
+		if st == false then
+			return false
+		end
+		coroutine.yield()
+	end
+end
+
+function maze:generate()
+	return coroutine.create(function() return generate(self) end)
+end
+
 return maze
